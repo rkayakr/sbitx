a5c61f9803c2e430c56715af2633f9802de1c5b8
jj <155218248+drexjj@users.noreply.github.com>
2024-07-01 09:04:10 -0500

    revert

--- sbitx.c
+++ sbitx.c
@@ -21,9 +21,9 @@
 #include "i2cbb.h"
 #include "si5351.h"
 #include "ini.h"
-//#include "qro.h" //added for QRO update W2JON
+
 #define DEBUG 0
-int ext_ptt_enable = 0; //ADDED BY KF7YDU.  Can be used by external software to enable/disable external PTT.  1=enabled, 0=disabled. Disabled by default.
+
 char audio_card[32];
 static int tx_shift = 512;
 
@@ -79,7 +79,7 @@ static double spectrum_speed = 0.3;
 static int in_tx = 0;
 static int rx_tx_ramp = 0;
 static int sidetone = 2000000000;
-struct vfo tone_a, tone_b, am_carrier; //these are audio tone generators
+struct vfo tone_a, tone_b; //these are audio tone generators
 static int tx_use_line = 0;
 struct rx *rx_list = NULL;
 struct rx *tx_list = NULL;
@@ -89,7 +89,7 @@ static double alc_level = 1.0;
 static int tr_relay = 0;
 static int rx_pitch = 700; //used only to offset the lo for CW,CWR
 static int bridge_compensation = 100;
-static double voice_clip_level = 0.04;
+static double voice_clip_level = 0.022;
 static int in_calibration = 1; // this turns off alc, clipping et al
 
 static int multicast_socket = -1;
@@ -413,7 +413,7 @@ void tx_init(int frequency, short mode, int bpf_low, int bpf_high){
 	//the tuning can go up and down only by 22 KHz from the center_freq
 
 	tx_filter = filter_new(1024, 1025);
-//	filter_tune(tx_filter, (1.0 * bpf_low)/96000.0, (1.0 * bpf_high)/96000.0 , 5);
+	filter_tune(tx_filter, (1.0 * bpf_low)/96000.0, (1.0 * bpf_high)/96000.0 , 5);
 }
 
 struct rx *add_tx(int frequency, short mode, int bpf_low, int bpf_high){
@@ -552,21 +552,28 @@ double agc2(struct rx *r){
 		r->agc_gain = agc_gain_should_be;
 		//reset the agc to hang count down 
     r->agc_loop = r->agc_speed;
+//  	printf("attack %g %d ", r->agc_gain, r->agc_loop);
   }
 	else if (r->agc_loop <= 0){
 		agc_ramp = (agc_gain_should_be - r->agc_gain) / (MAX_BINS/2);	
+//  	printf("release %g %d ",  r->agc_gain, r->agc_loop);
 	}
+//	else if (r->agc_loop > 0)
+//  	printf("hanging %g %d ", r->agc_gain, r->agc_loop);
  
 	if (agc_ramp != 0){
+//		printf("Ramping from %g ", r->agc_gain);
   	for (i = 0; i < MAX_BINS/2; i++){
 	  	__imag__ (r->fft_time[i+(MAX_BINS/2)]) *= r->agc_gain;
 		}
 		r->agc_gain += agc_ramp;		
+//		printf("by %g to %g ", agc_ramp, r->agc_gain);
 	}
 	else 
   	for (i = 0; i < MAX_BINS/2; i++)
 	  	__imag__ (r->fft_time[i+(MAX_BINS/2)]) *= r->agc_gain;
 
+//	printf("\n");
   r->agc_loop--;
 
 	//printf("%d:s meter: %d %d %d \n", count++, (int)r->agc_gain, (int)r->signal_strength, r->agc_loop);
@@ -577,100 +584,13 @@ void my_fftw_execute(fftw_plan f){
 	fftw_execute(f);
 }
 
-static int32_t rx_am_avg = 0;
-
-void rx_am(int32_t *input_rx,  int32_t *input_mic, 
-	int32_t *output_speaker, int32_t *output_tx, int n_samples)
-{
-	int i, j = 0;
-	double i_sample, q_sample;
-	//STEP 1: first add the previous M samples to
-	for (i = 0; i < MAX_BINS/2; i++)
-		fft_in[i]  = fft_m[i];
-
-	//STEP 2: then add the new set of samples
-	// m is the index into incoming samples, starting at zero
-	// i is the index into the time samples, picking from 
-	// the samples added in the previous step
-	int m = 0;
-	//gather the samples into a time domain array 
-	for (i= MAX_BINS/2; i < MAX_BINS; i++){
-		i_sample = (1.0  *input_rx[j])/200000000.0;
-		q_sample = 0;
-
-		j++;
-
-		__real__ fft_m[m] = i_sample;
-		__imag__ fft_m[m] = q_sample;
-
-		__real__ fft_in[i]  = i_sample;
-		__imag__ fft_in[i]  = q_sample;
-		m++;
-	}
-
-	// STEP 3: convert the time domain samples to  frequency domain
-	my_fftw_execute(plan_fwd);
-
-	//STEP 3B: this is a side line, we use these frequency domain
-	// values to paint the spectrum in the user interface
-	// I discovered that the raw time samples give horrible spectrum
-	// and they need to be multiplied wiht a window function 
-	// they use a separate fft plan
-	// NOTE: the spectrum update has nothing to do with the actual
-	// signal processing. If you are not showing the spectrum or the
-	// waterfall, you can skip these steps
-	for (i = 0; i < MAX_BINS; i++)
-		__real__ fft_in[i] *= spectrum_window[i];
-	my_fftw_execute(plan_spectrum);
-
-	// the spectrum display is updated
-	spectrum_update();
-	
-	struct rx *r = rx_list;
-
-	//STEP 4: we rotate the bins around by r-tuned_bin
-	for (i = 0; i < MAX_BINS; i++){
-		int b =  i + r->tuned_bin;
-		if (b >= MAX_BINS)
-			b = b - MAX_BINS;
-		if (b < 0)
-			b = b + MAX_BINS;
-		r->fft_freq[i] = fft_out[b]; 
-//		r->fft_freq[i] = fft_out[i];
-	}
-
-	// STEP 6: apply the filter to the signal,
-	// in frequency domain we just multiply the filter
-	// coefficients with the frequency domain samples
-	for (i = 0; i < MAX_BINS; i++)
-		r->fft_freq[i] *= r->filter->fir_coeff[i];
-
-	//STEP 7: convert back to time domain	
-	my_fftw_execute(r->plan_rev);
-	//STEP 8 : AGC
-	agc2(r);
-
-	//do an independent am detection (this takes 12 khz of b/w)
-	for (i= MAX_BINS/2; i < MAX_BINS; i++){
-		int32_t sample;
-		sample = abs(r->fft_time[i]) * 1000000;
-		rx_am_avg = (rx_am_avg* 5 + sample)/6;
-		//keep transmit buffer empty
-		output_speaker[i] = sample;
-//		output_speaker[i] = abs(input_rx[i]);
-		output_tx[i] = 0;
-	}
-//	for (i = 0; i < n_samples; i++)
-//		output_speaker[i] = rx_am_avg = ((rx_am_avg * 9) + abs(input_rx[i]))/10;
-}
 
 //TODO : optimize the memory copy and moves to use the memcpy
-void rx_linear(int32_t *input_rx,  int32_t *input_mic, 
+void rx_process(int32_t *input_rx,  int32_t *input_mic, 
 	int32_t *output_speaker, int32_t *output_tx, int n_samples)
 {
 	int i, j = 0;
 	double i_sample, q_sample;
-
 
 	//STEP 1: first add the previous M samples to
 	for (i = 0; i < MAX_BINS/2; i++)
@@ -708,11 +628,12 @@ void rx_linear(int32_t *input_rx,  int32_t *input_mic,
 	// signal processing. If you are not showing the spectrum or the
 	// waterfall, you can skip these steps
 	for (i = 0; i < MAX_BINS; i++)
-		__real__ fft_in[i] *= spectrum_window[i];
+			__real__ fft_in[i] *= spectrum_window[i];
 	my_fftw_execute(plan_spectrum);
 
 	// the spectrum display is updated
 	spectrum_update();
+
 
 	// ... back to the actual processing, after spectrum update  
 
@@ -720,13 +641,10 @@ void rx_linear(int32_t *input_rx,  int32_t *input_mic,
 	// hence, the linkced list of receivers here
 	// at present, we handle just the first receiver
 	struct rx *r = rx_list;
-
+	
 	//STEP 4: we rotate the bins around by r-tuned_bin
-	int shift = r->tuned_bin;
-	if (r->mode == MODE_AM)
-		shift = 0;
 	for (i = 0; i < MAX_BINS; i++){
-		int b =  i + shift;
+		int b =  i + r->tuned_bin;
 		if (b >= MAX_BINS)
 			b = b - MAX_BINS;
 		if (b < 0)
@@ -734,14 +652,13 @@ void rx_linear(int32_t *input_rx,  int32_t *input_mic,
 		r->fft_freq[i] = fft_out[b];
 	}
 
-
 	// STEP 5:zero out the other sideband
 	if (r->mode == MODE_LSB || r->mode == MODE_CWR)
 		for (i = 0; i < MAX_BINS/2; i++){
 			__real__ r->fft_freq[i] = 0;
 			__imag__ r->fft_freq[i] = 0;	
 		}
-	else if (r->mode != MODE_AM)  
+	else  
 		for (i = MAX_BINS/2; i < MAX_BINS; i++){
 			__real__ r->fft_freq[i] = 0;
 			__imag__ r->fft_freq[i] = 0;	
@@ -763,22 +680,13 @@ void rx_linear(int32_t *input_rx,  int32_t *input_mic,
 	int is_digital = 0;
 
 	if (rx_list->output == 0){
-		if (r->mode == MODE_AM)
-			for (i= 0; i < MAX_BINS/2; i++){
-				int32_t sample;
-				sample = cabs(r->fft_time[i+(MAX_BINS/2)]);
-				//keep transmit buffer empty
-				output_speaker[i] = sample;
-				output_tx[i] = 0;
-			}
-		else
-			for (i= 0; i < MAX_BINS/2; i++){
-				int32_t sample;
-				sample = cimag(r->fft_time[i+(MAX_BINS/2)]);
-				//keep transmit buffer empty
-				output_speaker[i] = sample;
-				output_tx[i] = 0;
-			}
+		for (i= 0; i < MAX_BINS/2; i++){
+			int32_t sample;
+			sample = cimag(r->fft_time[i+(MAX_BINS/2)]);
+			//keep transmit buffer empty
+			output_speaker[i] = sample;
+			output_tx[i] = 0;
+		}
 
 		//push the samples to the remote audio queue, decimated to 16000 samples/sec
 		for (i = 0; i < MAX_BINS/2; i += 6)
@@ -842,7 +750,7 @@ void tx_process(
 	int n_samples)
 {
 	int i;
-	double i_sample, q_sample, i_carrier;
+	double i_sample, q_sample;
 
 	struct rx *r = tx_list;
 
@@ -852,8 +760,7 @@ void tx_process(
 		tx_process_restart = 0;
 	} 
 
-	if (mute_count && (r->mode == MODE_USB || r->mode == MODE_LSB 
-		|| r->mode == MODE_AM)){
+	if (mute_count && (r->mode == MODE_USB || r->mode == MODE_LSB)){
 		memset(input_mic, 0, n_samples * sizeof(int32_t));
 		mute_count--;
 	}
@@ -870,46 +777,44 @@ void tx_process(
 
 		if (r->mode == MODE_2TONE)
 			i_sample = (1.0 * (vfo_read(&tone_a) 
-			+ vfo_read(&tone_b))) / 50000000000.0;
+										+ vfo_read(&tone_b))) / 50000000000.0;
 		else if (r->mode == MODE_CALIBRATE)
-			i_sample = (1.0 * (vfo_read(&tone_a))) / 30000000000.0;
+			i_sample = (1.0 * (vfo_read(&tone_a))) / 25000000000.0;
 		else if (r->mode == MODE_CW || r->mode == MODE_CWR || r->mode == MODE_FT8)
 			i_sample = modem_next_sample(r->mode) / 3;
-		else if (r->mode == MODE_AM){
-			//double modulation = (1.0 * vfo_read(&tone_a)) / 1073741824.0;
-	  		double modulation = (1.0 * input_mic[j]) / 200000000.0;
-			if (modulation < -1.0)
-				modulation = -1.0;
-			i_carrier= (1.0  * vfo_read(&am_carrier))/ 50000000000.0 ; 
-	  		i_sample =  (1.0 + modulation) * i_carrier;
+		else {
+	  	i_sample = (1.0 * input_mic[j]) / 2000000000.0;
 		}
-		else 
-	  		i_sample = (1.0 * input_mic[j]) / 2000000000.0;
-		
 		//clip the overdrive to prevent damage up the processing chain, PA
-		if (r->mode == MODE_USB || r->mode == MODE_LSB || r->mode == MODE_AM){
+		if (r->mode == MODE_USB || r->mode == MODE_LSB){
 			if (i_sample < (-1.0 * voice_clip_level))
 				i_sample = -1.0 * voice_clip_level;
 			else if (i_sample > voice_clip_level)
 				i_sample = voice_clip_level;
 		}
-
+/*
+		//to measure the voice peaks, used to measure voice_clip_level 
+		if (max < i_sample)
+			max = i_sample;
+		if (min > i_sample)
+			min = i_sample;
+*/
 		//don't echo the voice modes
 		if (r->mode == MODE_USB || r->mode == MODE_LSB || r->mode == MODE_AM 
 			|| r->mode == MODE_NBFM)
 			output_speaker[j] = 0;
 		else
 			output_speaker[j] = i_sample * sidetone;
-	  	q_sample = 0;
+	  q_sample = 0;
 
-	  	j++;
+	  j++;
 
-	  	__real__ fft_m[m] = i_sample;
-	  	__imag__ fft_m[m] = q_sample;
+	  __real__ fft_m[m] = i_sample;
+	  __imag__ fft_m[m] = q_sample;
 
-	  	__real__ fft_in[i]  = i_sample;
-	  	__imag__ fft_in[i]  = q_sample;
-	  	m++;
+	  __real__ fft_in[i]  = i_sample;
+	  __imag__ fft_in[i]  = q_sample;
+	  m++;
 	}
 
 	//push the samples to the remote audio queue, decimated to 16000 samples/sec
@@ -939,7 +844,7 @@ void tx_process(
 			__real__ fft_out[i] = 0;
 			__imag__ fft_out[i] = 0;	
 		}
-	else if (r->mode != MODE_AM)
+	else
 		// zero out the USB
 		for (i = MAX_BINS/2; i < MAX_BINS; i++){
 			__real__ fft_out[i] = 0;
@@ -947,12 +852,8 @@ void tx_process(
 		}
 
 	//now rotate to the tx_bin 
-	//rememeber the AM is already a carrier modulated at 24 KHz
-	int shift = tx_shift;
-	if (r->mode == MODE_AM)
-		shift = 0;
 	for (i = 0; i < MAX_BINS; i++){
-		int b = i + shift;
+		int b = i + tx_shift;
 		if (b >= MAX_BINS)
 			b = b - MAX_BINS;
 		if (b < 0)
@@ -995,31 +896,24 @@ void sound_process(
 	if (in_tx)
 		tx_process(input_rx, input_mic, output_speaker, output_tx, n_samples);
 	else
-			rx_linear(input_rx, input_mic, output_speaker, output_tx, n_samples);
+		rx_process(input_rx, input_mic, output_speaker, output_tx, n_samples);
+
 	if (pf_record)
 		wav_record(in_tx == 0 ? output_speaker : input_mic, n_samples);
 }
 
 
 void set_rx_filter(){
-	//on AM filter at the IF level, instead of the baseband
-	if (rx_list->mode == MODE_AM){
-		printf("Setting AM filter\n");
-   		filter_tune(rx_list->filter, 
-      		(1.0 * (24000 - rx_list->high_hz))/96000.0 , 
-      		(1.0 * (24000 + rx_list->high_hz))/96000.0 , 
-      	5);
-	}
-	else if(rx_list->mode == MODE_LSB || rx_list->mode == MODE_CWR)
-    		filter_tune(rx_list->filter, 
-      		(1.0 * -rx_list->high_hz)/96000.0, 
-      		(1.0 * -rx_list->low_hz)/96000.0 , 
-      	5);
+	if(rx_list->mode == MODE_LSB || rx_list->mode == MODE_CWR)
+    filter_tune(rx_list->filter, 
+      (1.0 * -rx_list->high_hz)/96000.0, 
+      (1.0 * -rx_list->low_hz)/96000.0 , 
+      5);
 	else
-		filter_tune(rx_list->filter, 
-		(1.0 * rx_list->low_hz)/96000.0, 
-		(1.0 * rx_list->high_hz)/96000.0 , 
-     		 5);
+    filter_tune(rx_list->filter, 
+      (1.0 * rx_list->low_hz)/96000.0, 
+      (1.0 * rx_list->high_hz)/96000.0 , 
+      5);
 }
 
 /* 
@@ -1221,11 +1115,7 @@ void tr_switch_de(int tx_on){
 			sound_mixer(audio_card, "Master", 0);
 			sound_mixer(audio_card, "Capture", 0);
 			delay(1);
-			//ADDED BY KF7YDU - Check if ptt is enabled, if so, set ptt pin to high
-			if (ext_ptt_enable == 1) {
-				digitalWrite(EXT_PTT, HIGH);
-			}
-			delay(20);
+
 			//now switch of the signal back
 			//now ramp up after 5 msecs
 			delay(2);
@@ -1267,7 +1157,6 @@ void tr_switch_de(int tx_on){
 
 			//drive the tx line low, switching the signal path 
 			digitalWrite(TX_LINE, LOW);
-			digitalWrite(EXT_PTT, LOW); //ADDED by KF7YDU, shuts down ext_ptt.  If ext_ptt_enable is 0, the pin won't be high to begin with and here we just repeat pin low, so it would do nothing.
 			delay(5); 
 			//audio codec is back on
 			sound_mixer(audio_card, "Master", rx_vol);
@@ -1297,13 +1186,6 @@ void tr_switch_v2(int tx_on){
 			delay(2);
 			mute_count = 20;
 			tx_process_restart = 1;
-			
-			//ADDED BY KF7YDU - Check if ptt is enabled, if so, set ptt pin to high
-			if (ext_ptt_enable == 1) {
-				digitalWrite(EXT_PTT, HIGH);
-			}
-				delay(20);
-			
 			digitalWrite(TX_LINE, HIGH);
       delay(20);
 			set_tx_power_levels();
@@ -1330,7 +1212,6 @@ void tr_switch_v2(int tx_on){
 			delay(10);
 			//power down the PA chain to null any gain
 			digitalWrite(TX_LINE, LOW);
-			digitalWrite(EXT_PTT, LOW); //ADDED by KF7YDU, shuts down ext_ptt.  If ext_ptt_enable is 0, the pin won't be high to begin with and here we just repeat pin low, so it would do nothing.
 			delay(5); 
 			//audio codec is back on
 			sound_mixer(audio_card, "Master", rx_vol);
@@ -1361,16 +1242,14 @@ void setup(){
 	//setup the LPF and the gpio pins
 	pinMode(TX_LINE, OUTPUT);
 	pinMode(TX_POWER, OUTPUT);
-	pinMode(EXT_PTT, OUTPUT); //ADDED BY KF7YDU
 	pinMode(LPF_A, OUTPUT);
 	pinMode(LPF_B, OUTPUT);
 	pinMode(LPF_C, OUTPUT);
 	pinMode(LPF_D, OUTPUT);
-	digitalWrite(LPF_A, LOW);
-	digitalWrite(LPF_B, LOW);
-	digitalWrite(LPF_C, LOW);
-	digitalWrite(LPF_D, LOW);
-	digitalWrite(EXT_PTT, LOW); //ADDED BY KF7YDU - initialize ext_ptt to low at startup
+  digitalWrite(LPF_A, LOW);
+  digitalWrite(LPF_B, LOW);
+  digitalWrite(LPF_C, LOW);
+  digitalWrite(LPF_D, LOW);
 	digitalWrite(TX_LINE, LOW);
 	digitalWrite(TX_POWER, LOW);
 
@@ -1401,10 +1280,9 @@ void setup(){
 
 	vfo_start(&tone_a, 700, 0);
 	vfo_start(&tone_b, 1900, 0);
-	vfo_start(&am_carrier, 24000, 0);
 
 	delay(2000);	
-//	pf_debug = fopen("am_test.raw", "w");	
+
 }
 
 void sdr_request(char *request, char *response){
@@ -1441,13 +1319,15 @@ void sdr_request(char *request, char *response){
 			rx_list->mode = MODE_2TONE;
 		else if (!strcmp(value, "FT8"))
 			rx_list->mode = MODE_FT8;
-		else if (!strcmp(value, "AM"))
-			rx_list->mode = MODE_AM;
+		else if (!strcmp(value, "PSK31"))
+			rx_list->mode = MODE_PSK31;
+		else if (!strcmp(value, "RTTY"))
+			rx_list->mode = MODE_RTTY;
 		else
 			rx_list->mode = MODE_USB;
 		
-    		//set the tx mode to that of the rx1
-    		tx_list->mode = rx_list->mode;
+    //set the tx mode to that of the rx1
+    tx_list->mode = rx_list->mode;
 
 		// An interesting but non-essential note:
 		// the sidebands inverted twice, to come out correctly after all
@@ -1459,19 +1339,12 @@ void sdr_request(char *request, char *response){
 		// converts to a second IF of 26.999 - 27.025 = 26 KHz
 		// Effectively, if a signal moves up, so does the second IF
 
-		if (rx_list->mode == MODE_AM){
-			puts("\n\n\ntx am filter ");
-			filter_tune(tx_list->filter, 
-				(1.0 * 20000)/96000.0, 
-				(1.0 * 30000)/96000.0 , 
+		if (rx_list->mode == MODE_LSB || rx_list->mode == MODE_CWR){
+			filter_tune(rx_list->filter, 
+				(1.0 * -3000)/96000.0, 
+				(1.0 * -300)/96000.0 , 
 				5);
-			filter_tune(tx_filter, 
-				(1.0 * 20000)/96000.0, 
-				(1.0 * 30000)/96000.0 , 
-				5);
-		}
-		else if (rx_list->mode == MODE_LSB || rx_list->mode == MODE_CWR){
-			puts("\n\n\ntx LSB filter ");
+			//puts("\n\n\ntx filter ");
 			filter_tune(tx_list->filter, 
 				(1.0 * -3000)/96000.0, 
 				(1.0 * -300)/96000.0 , 
@@ -1482,7 +1355,10 @@ void sdr_request(char *request, char *response){
 				5);
 		}
 		else { 
-			puts("\n\n\ntx USB filter ");
+			filter_tune(rx_list->filter, 
+				(1.0 * 300)/96000.0, 
+				(1.0 * 3000)/96000.0 , 
+				5);
 			filter_tune(tx_list->filter, 
 				(1.0 * 300)/96000.0, 
 				(1.0 * 3000)/96000.0 , 
